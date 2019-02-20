e9240a742376ab5ffe910e4c1c6e3305cf314d0d
David Watkins <djrival7@gmail.com>
2019-02-20 10:54:32 -0500

    Code cleanup

--- README.md
+++ README.md
@@ -15,6 +15,8 @@ If you are using this code, please cite our work:
 
 **Robot Arm Pose Estimation through Pixel-Wise Part Classification.** *Jeannette Bohg, Javier Romero, Alexander Herzog and Stefan Schaal. Proceedings of the 2014 IEEE International Conference on Robotics and Automation. pp 3143--3150. 2014.*
 
+Code borrowed from: https://github.com/rogersce/cnpy
+
 Requirements
 ----------
 The following libraries are required to compile the code:
@@ -30,12 +32,104 @@ The following libraries are optional:
 * OpenMP (Parallelization)
 * PCL (Point cloud  I/O)
 
+Setup
+------------
+Install OpenCV 3 on Ubuntu 16.04. Adapted from [here](https://www.learnopencv.com/install-opencv3-on-ubuntu/)
+
+##### Step 1: Install OS libraries
+
+```bash
+sudo apt-get install build-essential checkinstall cmake pkg-config yasm
+sudo apt-get install git gfortran
+sudo apt-get install libjpeg8-dev libjasper-dev libpng12-dev
+ 
+# If you are using Ubuntu 14.04
+sudo apt-get install libtiff4-dev
+# If you are using Ubuntu 16.04
+sudo apt-get install libtiff5-dev
+ 
+sudo apt-get install libavcodec-dev libavformat-dev libswscale-dev libdc1394-22-dev
+sudo apt-get install libxine2-dev libv4l-dev
+sudo apt-get install libgstreamer0.10-dev libgstreamer-plugins-base0.10-dev
+sudo apt-get install qt5-default libgtk2.0-dev libtbb-dev
+sudo apt-get install libatlas-base-dev
+sudo apt-get install libfaac-dev libmp3lame-dev libtheora-dev
+sudo apt-get install libvorbis-dev libxvidcore-dev
+sudo apt-get install libopencore-amrnb-dev libopencore-amrwb-dev
+sudo apt-get install x264 v4l-utils
+ 
+# Optional dependencies
+sudo apt-get install libprotobuf-dev protobuf-compiler
+sudo apt-get install libgoogle-glog-dev libgflags-dev
+sudo apt-get install libgphoto2-dev libeigen3-dev libhdf5-dev doxygen
+```
+
+##### Step 2: Compile and Install OpenCV3
+
+```bash
+mkdir -p $HOME/dependencies
+cd $HOME/dependencies
+git clone https://github.com/opencv/opencv.git -b 3.3.1
+git clone https://github.com/opencv/opencv_contrib.git -b 3.3.1
+cd opencv
+mkdir build
+cd build
+cmake -D CMAKE_BUILD_TYPE=RELEASE \
+      -D CMAKE_INSTALL_PREFIX=/usr/local \
+      -D INSTALL_C_EXAMPLES=ON \
+      -D INSTALL_PYTHON_EXAMPLES=ON \
+      -D WITH_TBB=ON \
+      -D WITH_V4L=ON \
+      -D WITH_QT=ON \
+      -D WITH_OPENGL=ON \
+      -D OPENCV_EXTRA_MODULES_PATH=../../opencv_contrib/modules \
+      -D BUILD_EXAMPLES=ON ..
+make -j$(nproc)
+sudo make install -j$(nproc)
+sudo sh -c 'echo "/usr/local/lib" >> /etc/ld.so.conf.d/opencv.conf'
+sudo ldconfig
+``` 
+
+##### Step 3: Create symlink in virtual environment
+
+```bash
+$ find /usr/local/lib/ -type f -name "cv2*.so"
+############ For Python 2 ############
+## binary installed in dist-packages
+/usr/local/lib/python2.6/dist-packages/cv2.so
+/usr/local/lib/python2.7/dist-packages/cv2.so
+## binary installed in site-packages
+/usr/local/lib/python2.6/site-packages/cv2.so
+/usr/local/lib/python2.7/site-packages/cv2.so
+  
+############ For Python 3 ############
+## binary installed in dist-packages
+/usr/local/lib/python3.5/dist-packages/cv2.cpython-35m-x86_64-linux-gnu.so
+/usr/local/lib/python3.6/dist-packages/cv2.cpython-36m-x86_64-linux-gnu.so
+## binary installed in site-packages
+/usr/local/lib/python3.5/site-packages/cv2.cpython-35m-x86_64-linux-gnu.so
+/usr/local/lib/python3.6/site-packages/cv2.cpython-36m-x86_64-linux-gnu.so
+```
+
+Double check the directory before running these commands:
+
+```bash
+############ For Python 2 ############
+cd $HOME/.local/lib/python2.7/site-packages
+ln -s /usr/local/lib/python2.7/dist-packages/cv2.so cv2.so
+  
+############ For Python 3 ############
+cd $HOME/.local/lib/python3.6/site-packages
+ln -s /usr/local/lib/python3.6/dist-packages/cv2.cpython-36m-x86_64-linux-gnu.so cv2.so
+```
+
 Compilation
 ------------
 
-The compilation of this code is tested on Ubuntu (12.04 precise and 12.10 quantal). The CMakeLists.txt file might need to be slightly adapted for correctly linking against PCL dependent on whether you installed it as a standalone library or with ROS.
+The compilation of this code is tested on Ubuntu (12.04 precise, 12.10 quantal, and 16.04 xenial). The CMakeLists.txt file might need to be slightly adapted for correctly linking against PCL dependent on whether you installed it as a standalone library or with ROS.
 
-```
+
+```bash
 mkdir build
 cd build
 cmake ..
@@ -46,7 +140,7 @@ Testing
 ------------
 There is a small test program that will render the simulated kinect measurements of a wheel at a number of slightly perturbed transformations relative to the camera.
 
-```
+```bash
 cd bin
 ./render_object wheel.obj
 ```
@@ -54,4 +148,5 @@ cd bin
 This should store a number of depth and labeled images in /tmp. If you have PCL installed, it also stores point clouds as pcl files.
 
 Point clouds generated from a simulated kinect measurement taken from a wheel in 10 slightly different poses. This measurement does not expose additional noise.
+
 ![](data/Wheels.png?raw=true)
--- include/render_kinect/simulate.h
+++ include/render_kinect/simulate.h
@@ -39,117 +39,109 @@
 
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <boost/algorithm/string/predicate.hpp>
 
-#ifdef HAVE_PCL
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 #include <pcl/io/pcd_io.h>
-#endif 
 
 #include <string.h>
 
 #include <render_kinect/kinectSimulator.h>
-
-static unsigned countf = 0;
+#include <render_kinect/cnpy.h>
 
 namespace render_kinect {
 
-  class Simulate {
-  public:
-  
-  Simulate(CameraInfo &cam_info, std::string object_name, std::string dot_path) 
-    : out_path_("/tmp/") 
-      {
-	// allocate memory for depth image
-	int w = cam_info.width;
-	int h = cam_info.height;
+	class Simulate {
+	public:
 
-	depth_im_ = cv::Mat(h, w, CV_32FC1);
-	scaled_im_ = cv::Mat(h, w, CV_32FC1);
+		Simulate(CameraInfo &cam_info, const std::string &object_name, const std::string &dot_path)
+		{
+			// allocate memory for depth image
+			int w = cam_info.width;
+			int h = cam_info.height;
 
-	object_model_ = new KinectSimulator(cam_info, object_name, dot_path);
+			depth_im_ = cv::Mat(h, w, CV_32FC1);
+			scaled_im_ = cv::Mat(h, w, CV_32FC1);
 
-	transform_ = Eigen::Affine3d::Identity();
+			object_model_ = new KinectSimulator(cam_info, object_name, dot_path);
 
-      }
+			transform_ = Eigen::Affine3d::Identity();
 
-    ~Simulate() {
-      delete object_model_;
-    }
+		}
 
-    void simulateMeasurement(const Eigen::Affine3d &new_tf, bool store_depth, bool store_label, bool store_pcd) {
-      countf++;
-      
-      // update old transform
-      transform_ = new_tf;
+		~Simulate() {
+			delete object_model_;
+		}
 
-      // simulate measurement of object and store in image, point cloud and labeled image
-      cv::Mat p_result;
-      object_model_->intersect(transform_, point_cloud_, depth_im_, labels_);
-      
-      // in case object is not in view, don't store any data
-      // However, if background is used, there will be points in the point cloud
-      // although they don't belong to the arm
-      int n_vis = 4000;
-      if(point_cloud_.rows<n_vis) {
-	std::cout << "Object not in view.\n";
-	return;
-      }
+		void simulateMeasurement(const Eigen::Affine3d &new_tf) {
+			// empty previous measurements
+			cloud_.clear();
 
-      // store on disk
-      if (store_depth) {
-	std::stringstream lD;
-	lD << out_path_ << "depth_orig" << std::setw(3) << std::setfill('0')
-	   << countf << ".png";
-	convertScaleAbs(depth_im_, scaled_im_, 255.0f);
-	cv::imwrite(lD.str().c_str(), scaled_im_);
-      }
+			// update old transform
+			transform_ = new_tf;
 
-      // store on disk
-      if (store_label) {
-	std::stringstream lD;
-	lD << out_path_ << "labels" << std::setw(3) << std::setfill('0')
-	   << countf << ".png";
-	cv::imwrite(lD.str().c_str(), labels_);
-      }
+			// simulate measurement of object and store in image, point cloud and labeled image
+			object_model_->intersect(transform_, point_cloud_, depth_im_, labels_);
 
-      //convert point cloud to pcl/pcd format
-      if (store_pcd) {
+			// in case object is not in view, don't store any data
+			// However, if background is used, there will be points in the point cloud
+			// although they don't belong to the arm
+			int n_vis = 4000;
+			if(point_cloud_.rows < n_vis) {
+				std::cout << "Object not in view.\n";
+				return;
+			}
 
-#ifdef HAVE_PCL
-	std::stringstream lD;
-	lD << out_path_ << "point_cloud" << std::setw(3)
-	   << std::setfill('0') << countf << ".pcd";
+			// Fill in the cloud data
+			cloud_.width = static_cast<uint32_t>(point_cloud_.rows);
+			cloud_.height = 1;
+			cloud_.is_dense = false;
+			cloud_.points.resize(cloud_.width * cloud_.height);
 
-	pcl::PointCloud<pcl::PointXYZ> cloud;
-	// Fill in the cloud data
-	cloud.width = point_cloud_.rows;
-	cloud.height = 1;
-	cloud.is_dense = false;
-	cloud.points.resize(cloud.width * cloud.height);
+			for (int i = 0; i < point_cloud_.rows; i++) {
+				const float* point = point_cloud_.ptr<float>(i);
+				cloud_.points[i].x = point[0];
+				cloud_.points[i].y = point[1];
+				cloud_.points[i].z = point[2];
+			}
+		}
 
-	for (int i = 0; i < point_cloud_.rows; i++) {
-	  const float* point = point_cloud_.ptr<float>(i);
-	  cloud.points[i].x = point[0];
-	  cloud.points[i].y = point[1];
-	  cloud.points[i].z = point[2];
-	}
-	
-	if (pcl::io::savePCDFileBinary(lD.str(), cloud) != 0)
-	  std::cout << "Couldn't store point cloud at " << lD.str() << std::endl;
+		void save_pcd(const std::string &out_file_path) {
+			if(cloud_.empty()) {
+				std::cout << "Point cloud is empty. Nothing to save" << std::endl;
+				return;
+			}
 
-#else
-	std::cout << "Couldn't store point cloud since PCL is not installed." << std::endl;
-#endif
-      }
-    }
+			assert(boost::algorithm::ends_with(outFilePath, ".pcd"));
 
-    KinectSimulator *object_model_;
-    cv::Mat depth_im_, scaled_im_, point_cloud_, labels_;
-    std::string out_path_;
-    Eigen::Affine3d transform_; 
+			if (pcl::io::savePCDFileBinary(out_file_path, cloud_) != 0)
+				std::cout << "Couldn't store point cloud at " << out_file_path << std::endl;
+		}
 
-  };
+		void save_labels(const std::string &out_file_path) {
+            assert(boost::algorithm::ends_with(out_file_path, ".png"));
+            cv::imwrite(out_file_path, labels_);
+		}
+
+        void save_depth(const std::string &out_file_path) {
+            assert(boost::algorithm::ends_with(out_file_path, ".png"));
+            convertScaleAbs(depth_im_, scaled_im_, 255.0f);
+            cv::imwrite(out_file_path, scaled_im_);
+		}
+
+		void save_transform(const std::string &out_file_path) {
+		    assert(boost::algorithm::ends_with(out_file_path, ".npy"));
+		    double* matrix = transform_.matrix().data();
+		    cnpy::npy_save(out_file_path, matrix, {4, 4}, "w");
+		}
+
+		KinectSimulator *object_model_;
+		cv::Mat depth_im_, scaled_im_, point_cloud_, labels_;
+		pcl::PointCloud<pcl::PointXYZ> cloud_;
+		Eigen::Affine3d transform_;
+
+	};
 
 } //namespace render_kinect
 #endif // SIMULATE_H
